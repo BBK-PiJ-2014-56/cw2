bf845ad520d99b528e62f7840e82ba1cbbdb7898
BBK-PiJ-2014-56 <jimjohn_thornton@James-Thorntons-MacBook-Pro-3.local>
2014-11-23 23:47:15 +0000

    sundays tinkering 2

--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -12,34 +12,25 @@
 		String[] addtest1 = {"1/2","+","1/3"}; 
 		Fraction addtestanswer1 = new Fraction(5,6);
 		test(CalcTest.calculation(addtest1) ,addtestanswer1,"error test 1");
-
+/*
 		//tests if calculator works with 2 operations
 		
 		String[] addtest2 = {"1/2","+","1/3","*","2"}; 
-		Fraction addtestanswer2 = new Fraction(5,3);
+		Fraction addtestanswer2 = new Fraction(10,6);
 		test(CalcTest.calculation(addtest2) ,addtestanswer2,"error test 2");
 
 		//tests if calculator works with 3 operations
 		
-		String[] addtest = {"1/2","+","1/3","*","2","/","3"}; 
-		Fraction addtestanswer = new Fraction(5,2);
-		test(CalcTest.calculation(addtest) ,addtestanswer,"error test 3");
-
-		//question = "1/3 + 1/2";
-		//Fraction a = new Fraction(5, 6);
-		
-		//a = FractionCalculator clear(a);
-		//System.out.println(a);
+		String[] addtest3 = {"1/2","+","1/3","*","2","/","3"}; 
+		Fraction addtestanswer3 = new Fraction(5,9);
+		test(CalcTest.calculation(addtest3) ,addtestanswer3,"error test 3");
 		
 		System.out.println(CalcTest.calculation(addtest1));
-		
-		
-		//test for correct input
-		/*testInput = "4/5 + 2/3";
-		Fraction a = new Fraction(22,15);
-		if(!FTtest.evaluate(testValue,testInput).equals(a)) System.out.println("Evaluation failed");
-		*/
-		
+		System.out.println(CalcTest.calculation(addtest2));
+		System.out.println(CalcTest.calculation(addtest3));
+		System.out.println(CalcTest.calculation(addtestanswer2));
+		System.out.println(CalcTest.calculation(addtestanswer3));
+*/
 		//launchCalc();
 		//FractionCalculator fractionMaker(1/2);
 		//test(question.launchCalc(),a,"error test 1");
